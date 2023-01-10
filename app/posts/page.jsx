c3de7557e53bae2b49e79636f2d2b6dae5f33fda
OzanOcak <ozanocak@gmail.com>
2023-01-09 22:43:27 -0500

    implementing search

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,19 +1,18 @@
-import SearchBlog from "../../components/SearchBlog";
+import Search from "../../components/Search";
 import getPostMetadata from "../../components/getPostMetadata";
 import PostPreview from "../../components/PostPreview";
+import { log } from "console";
 
 const BlogPage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
     <PostPreview key={post.slug} {...post} />
   ));
-
+  console.log(postMetadata);
   return (
     <div>
-      <SearchBlog />
-      <div className="grid grid-cols-1 max-w-[40rem]  mx-auto  gap-4">{postPreviews}</div>
+      <Search users={postMetadata} />
     </div>
-    
   );
 };
 
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,13 +4,8 @@ import { PostMetadata } from "./PostMetadata";
 const PostPreview = (props: PostMetadata) => {
   return (
     <Link href={`/posts/${props.slug}`}>
-    <div
-      className="p-4 rounded-md shadow-sm
-    bg-gray-300"
-    >
+    <div className="p-4 rounded-md shadow-sm bg-gray-300" >
       <p className="text-sm text-slate-400">{props.date}</p>
-
-      
       <h2 className=" text-gray-600 hover: mb-4">{props.title}</h2>
       <p className="text-slate-700">{props.subtitle}</p>
     </div>
