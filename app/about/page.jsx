8639a6b2995702c3464105741ddcccce09fb3f66
OzanOcak <ozanocak@gmail.com>
2023-01-11 21:16:38 -0500

    message template

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -121,7 +121,7 @@ const About = () => {
           <button
             type="submit"
             onClick={onSubmit}
-            className=" uppercase text-sm font-bold tracking-wide bg-blue-400 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline"
+            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             Send Message
           </button>
